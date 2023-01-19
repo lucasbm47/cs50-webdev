75685bc391378e946463a68719a310087652d91d
Your Name <you@example.com>
2023-01-19 00:15:18 -0300

    Created greet.html file and modified the views.py file for including this value

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request): # Request represents the user request variable
-    return HttpResponse("Hello world!")
+    return render(request, "hello/index.html")
 
 def greet(request, in_name):
     out_str = "Hello " + in_name.capitalize()
-    return HttpResponse(out_str)
+    cont = {"html_name": in_name.capitalize()} # html_name is between double curly braces in the hello/greet.html template
+    return render(request, "hello/greet.html", cont)
