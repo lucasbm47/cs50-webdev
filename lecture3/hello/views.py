311bb7f5932d2cc590d73cc30b904c44ac7258b1
Your Name <you@example.com>
2023-01-19 00:46:15 -0300

    Added static folder and first css file to the greet.html template

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def index(request): # Request represents the user request variable
     return render(request, "hello/index.html")
 
 def greet(request, in_name):
     out_str = "Hello " + in_name.capitalize()
-    cont = {"html_name": in_name.capitalize()} # html_name is between double curly braces in the hello/greet.html template
+    lucky_number = str(random.random()*10)
+
+    cont = {"html_name": in_name.capitalize(),
+            "html_lucky_number": lucky_number}
+
+    # html_name is between double curly braces in the hello/greet.html template and allows to render the value of a variable in the HTML
+    # The keys of the cont dictionary must match the names in the HTML template between curly braces
     return render(request, "hello/greet.html", cont)
